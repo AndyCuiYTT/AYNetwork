89f49852d00a09cbd7e3eeb5df3ea8cb7cfd12df
Andy <AndyCui931123@163.com>
2017-06-07 19:13:19 +0800

    完善 get,post 请求,添加文件下载方法(文件下载成功判断有问题)

--- AYNetwork_Swift/AYNetwork/AYNetworkManager.swift
+++ AYNetwork_Swift/AYNetwork/AYNetworkManager.swift
@@ -18,6 +18,8 @@
 import UIKit
 import Alamofire
 
+public typealias ayParams = [String : Any]
+
 class AYNetworkManager: NSObject {
     
     
@@ -29,7 +31,7 @@ class AYNetworkManager: NSObject {
     ///   - params: 请求参数
     ///   - result: 成功返回数据
     ///   - fail: 失败返回数据
-    static func ay_post(_ urlStr: String ,_ params: Dictionary<String, Any> , result: @escaping (Any)->Void , fail: @escaping (Any)->Void){
+    static func ay_post(_ urlStr: String, _ params: ayParams? = nil, result: @escaping (Any)->Void, fail: @escaping (Any)->Void){
         Alamofire.request(urlStr, method: .post, parameters: params, encoding: URLEncoding.default, headers: nil).responseJSON { (response) in
             if response.result.isSuccess{
                 result(response.result.value!)
@@ -47,7 +49,7 @@ class AYNetworkManager: NSObject {
     ///   - params: 请求参数
     ///   - result: 请求成功返回数据
     ///   - fail: 请求失败返回数据
-    static func ay_get(_ urlStr: String ,_ params: Dictionary<String, Any> , result: @escaping (Any)->Void , fail: @escaping (Any)->Void){
+    static func ay_get(_ urlStr: String, _ params: ayParams? = nil, result: @escaping (Any)->Void, fail: @escaping (Any)->Void){
         Alamofire.request(urlStr, method: .get, parameters: params, encoding: URLEncoding.default, headers: nil).responseJSON { (response) in
             if response.result.isSuccess{
                 result(response.result.value!)
@@ -58,7 +60,35 @@ class AYNetworkManager: NSObject {
     }
 
     
-    
+    /// 下载文件
+    ///
+    /// - Parameters:
+    ///   - urlStr: 文件地址
+    ///   - method: 请求方式
+    ///   - param: 请求参数
+    ///   - fileURL: 保存文件路径
+    ///   - progress: 下载进度
+    ///   - result: 成功返回数据
+    ///   - fail: 失败返回数据
+    static func ay_download(_ urlStr: String, _ method: HTTPMethod? = .get, _ param: ayParams? = nil, fileURL: URL, progress: @escaping (Any)->Void, result: @escaping (Any)->Void, fail: @escaping (Any)->Void) {
+        
+        let destination: DownloadRequest.DownloadFileDestination = { _, response in
+            return (fileURL.appendingPathComponent(response.suggestedFilename!), [.removePreviousFile, .createIntermediateDirectories])
+        }
+        
+        Alamofire.download(urlStr, method: method!, parameters: param, encoding: URLEncoding.default, headers: nil, to: destination).downloadProgress(queue: DispatchQueue.main, closure: { (progres) in
+            progress(progres)
+        }).responseJSON { (response) in
+                        
+            if response.response?.statusCode == 200 {
+                result(response.destinationURL!)
+            }else{
+                fail(response.error?.localizedDescription ?? "Error")
+            }
+            
+            
+        }
+    }
     
     
     
--- AYNetwork_Swift/AYNetwork_Swift/ViewController.swift
+++ AYNetwork_Swift/AYNetwork_Swift/ViewController.swift
@@ -19,15 +19,41 @@ class ViewController: UIViewController {
     }
     @IBAction func loadData(_ sender: UIButton) {
         
+//        
+//        AYNetworkManager.ay_get("http://localhost:8080/MavenDemo/userApi?method=getAllUsers", result: { (resultData) in
+//            print(resultData)
+//        }) { (failData) in
+//            print(failData)
+//        }
+//        
         
-        AYNetworkManager.ay_post("http://localhost:8080/MavenDemo/userApi1", ["method":"getAllUsers"], result: { (resultData) in
-            print(resultData)
+        AYNetworkManager.ay_download("https://github.com/AndyCuiYTT/AYWebImage/archive/master.zip", fileURL: URL.init(fileURLWithPath: self.ay_getCachePath()), progress: { (progress) in
+            
+            print(progress);
+            
+            
+        }, result: { (resultData) in
+            print(resultData);
         }) { (failData) in
             print(failData)
+            
         }
         
         
+    }
+    
+    
+    /// 获取图片缓存路径
+    ///
+    /// - Returns: 图片缓存路径
+    private func ay_getCachePath() -> String {
+        let fileStr: String = (NSSearchPathForDirectoriesInDomains(FileManager.SearchPathDirectory.cachesDirectory, FileManager.SearchPathDomainMask.userDomainMask, true).last?.appending("/imageCache"))!
+        let fileManager: FileManager = FileManager.default;
         
+        if !fileManager.fileExists(atPath: fileStr) {
+            try? fileManager.createDirectory(atPath: fileStr, withIntermediateDirectories: true, attributes: nil);
+        }
+        return fileStr
     }
 
     override func didReceiveMemoryWarning() {
